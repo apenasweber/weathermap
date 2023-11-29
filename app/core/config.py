0a6445c3445b5f84b220d134be742162e67579ef
Weber <andweber92@gmail.com>
2023-11-29 20:10:32 -0300

    feat: update services and main.py

--- app/core/config.py
+++ app/core/config.py
@@ -4,14 +4,14 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
-    DB_NAME: str
     DB_HOST: str
-    SECRET_KEY: str
     JWT_ALGORITHM: str
-    DATABASE_URL: str
     OPEN_WEATHER_API_KEY: str
-    OPEN_WEATHER_MAPS_FORECAST_URL = str
+    OPEN_WEATHER_MAPS_FORECAST_URL: str
     API_PORT: int
+    MONGO_URL: str
+    MONGO_DB_NAME: str
+    SECRET_KEY: str
 
     class Config:
         env_file = ".env"
--- app/services/open_weather_service.py
+++ app/services/open_weather_service.py
@@ -1,17 +1,18 @@
 from typing import Dict, List, Optional
 import requests
 import logging
-from app.core.config import Settings
+from app.db.weather_db import save_weather_data
+from app.models.open_weather_models import OpenWeatherResponse
+from app.core.config import settings
 
-# Configurando o logger
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 class OpenWeatherMapsAPI:
     def __init__(self) -> None:
-        self.open_weather_maps_forecast_url = Settings.OPEN_WEATHER_MAPS_FORECAST_URL
-        self.open_weather_maps_api_key: str = Settings.OPEN_WEATHER_API_KEY
+        self.open_weather_maps_forecast_url = settings.OPEN_WEATHER_MAPS_FORECAST_URL
+        self.open_weather_maps_api_key: str = settings.OPEN_WEATHER_API_KEY
 
     def get_weather_forecast_by_city(self, city: str) -> Optional[List[Dict]]:
         try:
@@ -43,9 +44,15 @@ class OpenWeatherMapsAPI:
     def request_open_weather_api(self, params: Dict) -> Optional[List[Dict]]:
         try:
             response = requests.get(self.open_weather_maps_forecast_url, params=params)
-            response.raise_for_status()  # Levanta uma exceção para respostas de erro HTTP
+            logger.info(response.text)
+            response.raise_for_status()
             data = response.json()
-            return data.get("list", [])
+            weather_data = OpenWeatherResponse(**data)
+
+            # Salva os dados no MongoDB
+            save_weather_data(weather_data)
+
+            return weather_data.list
         except requests.RequestException as e:
             logger.error(f"Erro na solicitação da API OpenWeather: {e}")
             return None
